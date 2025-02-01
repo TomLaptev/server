da19b2ba3af22ecd51c0d2beef93406d9d97016d
TomLaptev <laptev59ap@yandex.ru>
2025-02-01 14:52:04 +0700

    updatePlayers_2

--- server.js
+++ server.js
@@ -1,81 +1,79 @@
-const express = require("express");
-const { createServer } = require("http");
-const { Server } = require("socket.io");
-const cors = require("cors");
+const express = require('express');
+const { createServer } = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
 
 const app = express();
 const server = createServer(app);
 const io = new Server(server, {
-  cors: {
-    origin: "*", // Разрешает соединение откуда угодно, можно ограничить
-    methods: ["GET", "POST"],
-  },
+	cors: {
+		origin: [
+			'https://yandex.ru',
+			'https://yandex.ru/games',
+			'https://tictactoe-server-wuoa.onrender.com',
+		], // Разрешает соединение откуда угодно, можно ограничить
+		methods: ['GET', 'POST'],
+	},
 });
 
 app.use((req, res, next) => {
-  res.setHeader("Content-Security-Policy", 
-    "default-src 'self' https://tictactoe-server.onrender.com; " +
-    "script-src 'self' 'unsafe-inline' https://tictactoe-server.onrender.com; " +
-    "connect-src 'self' https://tictactoe-server.onrender.com ws://localhost:3000 wss://localhost:3000;"
-  );
-  next();
+	res.setHeader(
+		'Content-Security-Policy',
+		"default-src 'self' https://tictactoe-server.onrender.com; " +
+			"script-src 'self' 'unsafe-inline' https://tictactoe-server.onrender.com; " +
+			"connect-src 'self' https://tictactoe-server.onrender.com ws://localhost:3000 wss://localhost:3000;"
+	);
+	next();
 });
 
 const PORT = process.env.PORT || 3000;
 
 let players = {}; // Храним данные игроков
 
-io.on("connection", (socket) => {
-  console.log("Новое подключение:", socket.id);
+io.on('connection', (socket) => {
+	console.log('Новое подключение:', socket.id);
 
-  socket.on("playerJoin", (playerData) => {
-    players[socket.id] = playerData;
-    console.log("Присоединился игрок:", playerData);
+	socket.on('playerJoin', (playerData) => {
+		players[socket.id] = playerData;
+		console.log('Присоединился игрок:', playerData);
 
-    io.emit("updatePlayers", Object.values(players)); // Рассылаем всем
-  });
+		io.emit('updatePlayers', Object.values(players)); // Рассылаем всем
+	});
 
-  socket.on('requestPlayersList', () => {
-    console.log("Запрос на обновление");
+	socket.on('playerExit', () => {
+		console.log('Игрок отключился:', players[socket.id]);
+		delete players[socket.id];
+
+		io.emit('updatePlayers', Object.values(players)); // Обновляем список
+	});
+
+	socket.on('disconnect', () => {
+		console.log('Отключение:', socket.id);
+		delete players[socket.id];
+		io.emit('updatePlayers', Object.values(players));
+	});
+
+	// Получение списка игроков
+	socket.on('requestPlayers', () => {
+		io.to(socket.id).emit('updatePlayers', Object.values(players));
+    
     const playerList = getOnlinePlayers(); // Функция для получения списка игроков
-    io.emit('updatePlayersList', Object.values(players));
+    socket.emit('updatePlayersList', playerList);
+	});
+
+	// Отправка приглашения игроку
+	socket.on('invitePlayer', ({ opponentId }) => {
+		const roomId = `room-${socket.id}-${opponentId}`;
+	});
+
+	if (players[opponentId]) {
+		io.to(opponentId).emit('receiveInvite', { opponentId: socket.id, roomId });
+	}
 });
 
-  socket.on("playerExit", () => {
-    console.log("Игрок отключился:", players[socket.id]);
-    delete players[socket.id];
-
-    io.emit("updatePlayers", Object.values(players)); // Обновляем список
-  });
-
-  socket.on("disconnect", () => {
-    console.log("Отключение:", socket.id);
-    delete players[socket.id];
-    io.emit("updatePlayers", Object.values(players));
-  });
+app.get('/', (req, res) => {
+	res.send('Сервер работает!');
 });
-
-app.get("/", (req, res) => {
-  res.send("Сервер работает!");
-});
-
-
-// Получение списка игроков
-socket.on("requestPlayers", () => {
-  io.to(socket.id).emit("updatePlayers", Object.values(players));
-});
-
-// Отправка приглашения игроку
-socket.on("invitePlayer", ({ opponentId }) => {
-  const roomId = `room-${socket.id}-${opponentId}`;
-  
-  if (players[opponentId]) {
-      io.to(opponentId).emit("receiveInvite", { opponentId: socket.id, roomId });
-  }
-});
-
-
-
 server.listen(PORT, () => {
-  console.log(`Сервер запущен на порту ${PORT}`);
+	console.log(`Сервер запущен на порту ${PORT}`);
 });
