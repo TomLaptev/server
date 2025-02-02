c17a991782abe8e7a83609fc2c798234c44ee9a2
TomLaptev <laptev59ap@yandex.ru>
2025-02-02 11:01:17 +0700

    updatePlayers_7

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
 	cors: {
-		origin: "*", // Разрешает соединение откуда угодно, можно ограничить
+		origin: '*', // Разрешает соединение откуда угодно, можно ограничить
 		methods: ['GET', 'POST'],
 	},
 });
@@ -31,56 +31,56 @@ const PORT = process.env.PORT || 3000;
 let players = {}; // Храним данные игроков
 
 io.on('connection', (socket) => {
-	socket.on("playerConnected", ({ yandexId }) => {
+	socket.on('playerConnected', ({ yandexId }) => {
 		socket.yandexId = yandexId; // Сохраняем yandexId в сокете
 		players[yandexId] = { socketId: socket.id, opponent: null };
-	console.log(`Игрок с ID ${yandexId} подключен.`);
-});
+		console.log(`Игрок с ID ${yandexId} подключен.`);
+	});
 
 	socket.on('playerJoin', (playerData) => {
 		const { yandexId } = playerData; // Извлекаем yandexId из playerData
-    players[yandexId] = { ...playerData, socketId: socket.id };
+		players[yandexId] = { ...playerData, socketId: socket.id };
 		console.log('Присоединился игрок:', playerData);
 
 		io.emit('updatePlayers', Object.values(players)); // Рассылаем всем
 	});
 
 	socket.on('playerExit', () => {
-    const yandexId = socket.yandexId;
-    if (yandexId && players[yandexId]) {
-        console.log('Игрок отключился:', players[yandexId]);
-        delete players[yandexId];
-        io.emit('updatePlayers', Object.values(players));
-    }
-});
+		const yandexId = socket.yandexId;
+		if (yandexId && players[yandexId]) {
+			console.log('Игрок отключился:', players[yandexId]);
+			delete players[yandexId];
+			io.emit('updatePlayers', Object.values(players));
+		}
+	});
 
-socket.on('disconnect', () => {
-	const yandexId = socket.yandexId;
-	if (yandexId && players[yandexId]) {
+	socket.on('disconnect', () => {
+		const yandexId = socket.yandexId;
+		if (yandexId && players[yandexId]) {
 			console.log('Отключение:', yandexId);
 			delete players[yandexId];
 			io.emit('updatePlayers', Object.values(players));
-	}
-});
+		}
+	});
 
 	// Получение списка игроков
 	socket.on('requestPlayers', () => {
+		const yandexId = socket.yandexId;
 		io.to(yandexId).emit('updatePlayers', Object.values(players));
 
-   // const playerList = getOnlinePlayers(); // Функция для получения списка игроков
-   //io.emit('updatePlayersList', playerList);
+		// const playerList = getOnlinePlayers(); // Функция для получения списка игроков
+		//io.emit('updatePlayersList', playerList);
 	});
 
 	// Отправка приглашения игроку
-  socket.on('invitePlayer', ({ opponentId, user }) => {
-    if (players[user]) {
-        players[user].opponent = opponentId;
-    } else {
-        console.error(`Игрок с ID ${user} не найден в списке игроков.`);
-    }
-});
-
-
+	socket.on('invitePlayer', ({ opponentId, user }) => {
+		
+		if (players[user]) {
+			players[user].opponent = opponentId;
+		} else {
+			console.error(`Игрок с ID ${user} не найден в списке игроков.`);
+		}
+	});
 });
 
 app.get('/', (req, res) => {
