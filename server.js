2b048cc22a4201bf14b776067c6422a165d65cdf
TomLaptev <laptev59ap@yandex.ru>
2025-02-02 21:52:04 +0700

    updatePlayers_8

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ io.on('connection', (socket) => {
   socket.on('invitePlayer', ({ opponentId, user }) => {
     if (players[user]) {
         players[user].opponent = opponentId;
-        players[user].roomId = Key();
+        players[user].roomId = roomId;
     } else {
         console.error(`Игрок с ID ${user} не найден в списке игроков.`);
     }
