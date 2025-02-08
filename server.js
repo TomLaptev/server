b9235b6835574574751dc35a495334422fbbaca5
TomLaptev <laptev59ap@yandex.ru>
2025-02-08 12:51:51 +0700

    Удаление неактивного игрока

--- server.js
+++ server.js
@@ -149,17 +149,16 @@ io.on('connection', (socket) => {
 	// Обновление комнаты
 	socket.on('refusalPlay', (roomId) => {
 		if (rooms[socket.id]) {
-			io.to(roomId).emit('roomDelete', () => {
-				console.log('Приватная комната игрока-А удалена');
-			});
+			io.to(roomId).emit('roomDelete', rooms[socket.id] );
 			delete rooms[socket.id];
+			console.log('Приватная комната игрока-А удалена');
 		}
 		if (rooms[roomId]) {
-			io.to(roomId).emit('roomDelete', () => {
-				console.log('Приватная комната игрока-Б удалена');
-			});
+			io.to(roomId).emit('roomDelete', rooms[roomId]);
 			delete rooms[roomId];
+			console.log('Приватная комната игрока-Б удалена');
 		}
+
 	});
 });
 
