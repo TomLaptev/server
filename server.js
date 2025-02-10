cfa86b3bb74339b8d3adcbb8419784c6cff42a2c
TomLaptev <laptev59ap@yandex.ru>
2025-02-10 08:13:39 +0700

    Удаление неактивного игрока

--- server.js
+++ server.js
@@ -142,7 +142,7 @@ io.on('connection', (socket) => {
 			players[id].available = available;
 		}
 
-		//io.emit('updatePlayers', Object.values(players)); // Отправляем обновленный список
+		io.emit('updatePlayers', Object.values(players)); // Отправляем обновленный список
 	});
 
 	// Присоединение к комнате
