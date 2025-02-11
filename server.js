afad6949faf9686a742efdf3763764ceda9aea11
TomLaptev <laptev59ap@yandex.ru>
2025-02-11 08:07:18 +0700

    Обновление комнаты

--- server.js
+++ server.js
@@ -103,7 +103,7 @@ io.on('connection', (socket) => {
 	// Отправка приглашения игроку
 	socket.on('sendInvitePlayer', ({ roomId, opponentSocketId, name }) => {
 		io.to(opponentSocketId).emit('roomInvitation', { roomId });
-		io.to(roomId).emit('roomUpdate', rooms[roomId]);
+		//io.to(roomId).emit('roomUpdate', rooms[roomId]);
 		console.log(
 			`Приглашение отправлено игроку ${name} в комнату ${roomId}`
 		);
@@ -128,7 +128,7 @@ io.on('connection', (socket) => {
 			console.log(`Игрок ${players[socket.id].name} присоединился к комнате ${roomId}`);
 
 			// Уведомляем всех участников комнаты
-			//io.to(roomId).emit('roomUpdate', rooms[roomId]);
+			io.to(roomId).emit('roomUpdate', rooms[roomId]);
 		}
 	});
 
