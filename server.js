98c22274b07b90e27129ae0aea66500fb1d02637
TomLaptev <laptev59ap@yandex.ru>
2025-02-23 19:41:07 +0700

    Обмен данными

--- server.js
+++ server.js
@@ -93,8 +93,8 @@ io.on('connection', (socket) => {
 
 		// Удаляем игрока из списка
 		if (players[socket.id]) {
-			delete players[socket.id];
-			console.log(`Игрок ${socket.id} удалён из списка игроков`);
+			// delete players[socket.id];
+			// console.log(`Игрок ${socket.id} удалён из списка игроков`);
 
 			io.emit('updatePlayers', Object.values(players));
 			console.log('Контроль запроса на обновление');
